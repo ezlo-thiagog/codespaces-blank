dcabc27597e2d18a5371a47631bc5e8bbe5f9498
ezlo-thiagog <santiago.gutierrez@ezlo.com>
2025-07-21 16:32:33 +0000

    Added support for Tricon Residential Channel

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 
 const CONFIG = {
   METABASE_BASE_URL: "https://metabase.mios.com/public/question/a3c3b26f-1c43-48d4-9061-39dd872d1014",
-  ALLOWED_CHANNELS: ["C04SM9CP4F2", "C07KG1EU83H"]
+  ALLOWED_CHANNELS: ["C04SM9CP4F2", "C07KG1EU83H", "C0299MP0XC6"]
 };
 
 module.exports = CONFIG;
